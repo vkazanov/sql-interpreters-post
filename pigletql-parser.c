4a17ba580313c46528555ae6c5ac8a0508c698ad
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2019-03-26 08:39:40 +0000

    Prefix all the scanner functions

--- pigletql-parser.c
+++ pigletql-parser.c
@@ -36,47 +36,47 @@ void scanner_destroy(scanner_t *scanner)
         free(scanner);
 }
 
-static bool is_alpha(char c)
+static bool char_is_alpha(char c)
 {
     return (c >= 'a' && c <= 'z')
         || (c >= 'A' && c <= 'Z')
         || c == '_';
 }
 
-static bool is_digit(char c)
+static bool char_is_digit(char c)
 {
     return (c >= '0' && c <= '9');
 }
 
-static bool is_eos(scanner_t *scanner)
+static bool scanner_at_eos(scanner_t *scanner)
 {
     return *scanner->input == '\0';
 }
 
-static char peek(scanner_t *scanner)
+static char scanner_peek(scanner_t *scanner)
 {
     return tolower(*scanner->input);
 }
 
-static char peek_start(scanner_t *scanner)
+static char scanner_peek_start(scanner_t *scanner)
 {
     return tolower(*scanner->token_start);
 }
 
-static char advance(scanner_t *scanner)
+static char scanner_advance(scanner_t *scanner)
 {
     return *scanner->input++;
 }
 
-static void skip_space(scanner_t *scanner)
+static void scanner_skip_space(scanner_t *scanner)
 {
     for (;;) {
-        char c = peek(scanner);
+        char c = scanner_peek(scanner);
         switch (c) {
         case ' ':
         case '\r':
         case '\t':
-            advance(scanner);
+            scanner_advance(scanner);
             break;
         default:
             return;
@@ -84,7 +84,7 @@ static void skip_space(scanner_t *scanner)
     }
 }
 
-static token_t token_create(scanner_t *scanner, token_type type)
+static token_t scanner_token_create(scanner_t *scanner, token_type type)
 {
     token_t token;
     token.type = type;
@@ -93,7 +93,7 @@ static token_t token_create(scanner_t *scanner, token_type type)
     return token;
 }
 
-static token_t token_error_create(const char *error_msg)
+static token_t scanner_token_error_create(const char *error_msg)
 {
     token_t token;
     token.type = TOKEN_ERROR;
@@ -102,7 +102,7 @@ static token_t token_error_create(const char *error_msg)
     return token;
 }
 
-static token_type keyword(scanner_t *scanner, int start, int length, const char* suffix, token_type type)
+static token_type scan_keyword(scanner_t *scanner, int start, int length, const char* suffix, token_type type)
 {
     if (scanner->input - scanner->token_start != start + length)
         return TOKEN_IDENT;
@@ -115,57 +115,57 @@ static token_type keyword(scanner_t *scanner, int start, int length, const char*
     return type;
 }
 
-static token_type ident_type(scanner_t *scanner)
+static token_type scan_ident_type(scanner_t *scanner)
 {
-    switch(peek_start(scanner)) {
-    case 's': return keyword(scanner, 1, 5, "elect", TOKEN_SELECT);
-    case 'f': return keyword(scanner, 1, 3, "rom", TOKEN_FROM);
-    case 'w': return keyword(scanner, 1, 4, "here", TOKEN_WHERE);
+    switch(scanner_peek_start(scanner)) {
+    case 's': return scan_keyword(scanner, 1, 5, "elect", TOKEN_SELECT);
+    case 'f': return scan_keyword(scanner, 1, 3, "rom", TOKEN_FROM);
+    case 'w': return scan_keyword(scanner, 1, 4, "here", TOKEN_WHERE);
     }
     return TOKEN_IDENT;
 }
 
-static token_t ident(scanner_t *scanner)
+static token_t scanner_ident(scanner_t *scanner)
 {
-    while (is_digit(peek(scanner)) || is_alpha(peek(scanner)))
-        advance(scanner);
+    while (char_is_digit(scanner_peek(scanner)) || char_is_alpha(scanner_peek(scanner)))
+        scanner_advance(scanner);
 
-    return token_create(scanner, ident_type(scanner));
+    return scanner_token_create(scanner, scan_ident_type(scanner));
 }
 
-static token_t number(scanner_t *scanner)
+static token_t scanner_number(scanner_t *scanner)
 {
-    while (is_digit(peek(scanner)))
-        advance(scanner);
+    while (char_is_digit(scanner_peek(scanner)))
+        scanner_advance(scanner);
 
-    return token_create(scanner, TOKEN_NUMBER);
+    return scanner_token_create(scanner, TOKEN_NUMBER);
 }
 
 token_t scanner_next(scanner_t *scanner)
 {
-    skip_space(scanner);
+    scanner_skip_space(scanner);
 
     scanner->token_start = scanner->input;
 
-    if (is_eos(scanner))
-        return token_create(scanner, TOKEN_EOS);
+    if (scanner_at_eos(scanner))
+        return scanner_token_create(scanner, TOKEN_EOS);
 
-    char c = advance(scanner);
+    char c = scanner_advance(scanner);
 
-    if (is_alpha(c))
-        return ident(scanner);
+    if (char_is_alpha(c))
+        return scanner_ident(scanner);
 
-    if (is_digit(c))
-        return number(scanner);
+    if (char_is_digit(c))
+        return scanner_number(scanner);
 
     switch (c) {
-    case ';': return token_create(scanner, TOKEN_SEMICOLON);
-    case ',': return token_create(scanner, TOKEN_COMMA);
-    case '*': return token_create(scanner, TOKEN_STAR);
-    case '=': return token_create(scanner, TOKEN_EQUAL);
+    case ';': return scanner_token_create(scanner, TOKEN_SEMICOLON);
+    case ',': return scanner_token_create(scanner, TOKEN_COMMA);
+    case '*': return scanner_token_create(scanner, TOKEN_STAR);
+    case '=': return scanner_token_create(scanner, TOKEN_EQUAL);
     }
 
-    return token_error_create("Unknown character");
+    return scanner_token_error_create("Unknown character");
 }
 
 void parse(const char *query_string, query_t *query)

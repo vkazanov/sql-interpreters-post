f8924b9867493b87de861a21117554536d782676
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2019-05-15 08:37:52 +0100

    insertion tests

--- pigletql-validate-test.c
+++ pigletql-validate-test.c
@@ -81,6 +81,85 @@ static void create_validate_test(void)
 
 static void insert_validate_test(void)
 {
+    /* Insert into a non-existing table */
+    {
+        const char *query_str = "INSERT INTO rel1 VALUES (1, 2);";
+
+        catalogue_t *cat = catalogue_create();
+        scanner_t *scanner = scanner_create(query_str);
+        parser_t *parser = parser_create();
+        query_t *query = query_create();
+        assert(cat);
+        assert(scanner);
+        assert(parser);
+        assert(query);
+        assert(parser_parse(parser, scanner, query));
+
+        assert(!validate(cat, query));
+
+        query_destroy(query);
+        parser_destroy(parser);
+        scanner_destroy(scanner);
+        catalogue_destroy(cat);
+    }
+
+    /* Correct insertion */
+    {
+        const char *query_str = "INSERT INTO rel1 VALUES (1, 2, 3);";
+
+        catalogue_t *cat = catalogue_create();
+        {
+            const attr_name_t attr_names[] = {"id", "attr1", "attr2"};
+            const size_t attr_num = ARRAY_SIZE(attr_names);
+            relation_t *rel1 = relation_create(attr_names, attr_num);
+            catalogue_add_relation(cat, "rel1", rel1);
+        }
+
+        scanner_t *scanner = scanner_create(query_str);
+        parser_t *parser = parser_create();
+        query_t *query = query_create();
+        assert(cat);
+        assert(scanner);
+        assert(parser);
+        assert(query);
+        assert(parser_parse(parser, scanner, query));
+
+        assert(validate(cat, query));
+
+        query_destroy(query);
+        parser_destroy(parser);
+        scanner_destroy(scanner);
+        catalogue_destroy(cat);
+    }
+
+    /* Insert into a table with a different number of columns */
+    {
+        const char *query_str = "INSERT INTO rel1 VALUES (2, 3);";
+
+        catalogue_t *cat = catalogue_create();
+        {
+            const attr_name_t attr_names[] = {"id", "attr1", "attr2"};
+            const size_t attr_num = ARRAY_SIZE(attr_names);
+            relation_t *rel1 = relation_create(attr_names, attr_num);
+            catalogue_add_relation(cat, "rel1", rel1);
+        }
+
+        scanner_t *scanner = scanner_create(query_str);
+        parser_t *parser = parser_create();
+        query_t *query = query_create();
+        assert(cat);
+        assert(scanner);
+        assert(parser);
+        assert(query);
+        assert(parser_parse(parser, scanner, query));
+
+        assert(!validate(cat, query));
+
+        query_destroy(query);
+        parser_destroy(parser);
+        scanner_destroy(scanner);
+        catalogue_destroy(cat);
+    }
 
 }
 
